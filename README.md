56b11eb2cb2cb4d3e870fd6e6a7c69bd2609bbfd
xinxingyu <xinxingyu@meituan.com>
2018-05-02 17:42:13 +0800

    WORK: Build the foundation

---
+++ README.md
@@ -0,0 +1,3 @@
+# blog
+
+> 基于 vuepress 搭建的静态博客 [blog](https://xinxingyu.github.io/blog/)
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    base: '/blog/',
+    dest: './dist',
+    title: 'XXY BLOG',
+    description: 'xinxingyu blog',
+    themeConfig: {
+        repo: 'xinxingyu/blog',
+        // nav: [{}],
+        // sidebar: {
+
+        // }
+    }
+}
