2b9eb935e1e988b69c9f750068be66f2042324ac
xinxingyu <xinxingyu@meituan.com>
2018-05-11 17:50:58 +0800

    WORK: add theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,12 +2,56 @@ module.exports = {
     base: '/blog/',
     dest: './dist',
     title: 'XXY BLOG',
-    description: 'xinxingyu blog',
+    description: 'xinxingyu',
+    serviceWorker: true,
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: `/logo.jpeg`
+        }]
+    ],
+    // theme: 'vue',
     themeConfig: {
-        repo: 'xinxingyu/blog',
-        // nav: [{}],
-        // sidebar: {
-
-        // }
+        repo: 'xinxingyu',
+        nav: [{
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: 'blog',
+                link: '/blog/',
+                items: [{
+                        text: 'JavaScript',
+                        link: '/blog/JavaScript/'
+                    },
+                ]
+            },
+            {
+                text: 'About',
+                link: '/about/'
+            }
+        ],
+        sidebar: {
+            // '/blog/': genSidebarConfig('博客')
+        }
     }
+}
+
+
+function genSidebarConfig(title) {
+    return [{
+        title,
+        collapsable: false,
+        children: [
+            '',
+            'getting-started',
+            'basic-config',
+            'assets',
+            'markdown',
+            'using-vue',
+            'custom-themes',
+            'i18n',
+            'deploy'
+        ]
+    }]
 }
